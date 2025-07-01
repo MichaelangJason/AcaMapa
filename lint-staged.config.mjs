82cccead965345fda1fd58adf1b6104ba8b62b83
mkljs <mkl.js@outlook.com>
2025-06-30 23:29:04 -0400

    chore: prettier --write for pre-commit script

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,8 +1,8 @@
-
 export default {
-    "src/**/*.{js,ts,tsx,jsx}": (stagedFiles) => [
-        `gitleaks git --staged`,
-        `npx eslint ${stagedFiles.join(' ')}`,
-        `prettier --check ${stagedFiles.join(' ')}`
-    ]
-}
+  "src/**/*.{js,ts,tsx,jsx,css,scss}": (stagedFiles) => [
+    `gitleaks git --staged`,
+    `npx eslint ${stagedFiles.join(" ")}`,
+    `prettier --write ${stagedFiles.join(" ")}`,
+    `git add ${stagedFiles.join(" ")}`,
+  ],
+};
