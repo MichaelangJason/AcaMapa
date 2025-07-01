83abd16d830e6c591c7b6f4d8536d41a06311f07
mkljs <mkl.js@outlook.com>
2025-06-30 23:18:46 -0400

    chore: support directly import svg files, move some icons to public/icons/

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"], // https://stackoverflow.com/questions/65676689/next-js-how-can-i-change-the-color-of-svg-in-next-image
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
