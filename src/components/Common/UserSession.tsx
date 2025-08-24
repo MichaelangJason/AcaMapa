ad8a77735f90c884ddc09c2bade2b7710ba74909
mkljs <mkl.js@outlook.com>
2025-08-24 14:36:52 -0400

    chore: disable login redirect

--- src/components/Common/UserSession.tsx
+++ src/components/Common/UserSession.tsx
@@ -1,40 +1,40 @@
 "use client";
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useSession, signIn, signOut } from "next-auth/react";
 import ItemTag from "./ItemTag";
 import { useCallback, useMemo } from "react";
 import { I18nKey, Language, t } from "@/lib/i18n";
 import { useAppSelector } from "@/store/hooks";
 import { TooltipId } from "@/lib/enums";
+import { redirect } from "next/navigation";
 
 const UserSession = () => {
   const { data: session } = useSession();
   const lang = useAppSelector((state) => state.userData.lang) as Language;
 
-  const item = useMemo(() => {
-    if (session) {
-      return [t([I18nKey.LOGOUT], lang)];
-    }
-    return [];
-  }, [session, lang]);
+  // const item = useMemo(() => {
+  //   if (session) {
+  //     return [t([I18nKey.LOGOUT], lang)];
+  //   }
+  //   return [];
+  // }, [session, lang]);
 
-  const handleClickItem = useCallback(
-    (item: string) => {
-      if (item === t([I18nKey.LOGOUT], lang)) {
-        signOut();
-      } else {
-        signIn();
-      }
-    },
-    [lang],
-  );
+  // const handleClickItem = useCallback(
+  //   (item: string) => {
+  //     if (item === t([I18nKey.LOGOUT], lang)) {
+  //       signOut();
+  //     } else {
+  //       signIn();
+  //     }
+  //   },
+  //   [lang],
+  // );
 
   return (
     <ItemTag
-      items={item}
+      items={[]}
       title={session?.user?.email ?? t([I18nKey.LOGIN_WITH_MCGILL_EMAIL], lang)}
-      handleClickTag={session ? undefined : signIn}
-      handleClickItem={handleClickItem}
       isPinnable={false}
       style={{
         backgroundColor: "var(--mcgill-red)",
