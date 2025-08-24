f8bc48bc9489cc272c35a716828e32a86ecbd617
mkljs <mkl.js@outlook.com>
2025-08-24 11:43:24 -0400

    fix: workaround for link checker

--- src/app/api/[...nextauth]/route.ts
+++ src/app/api/[...nextauth]/route.ts
@@ -1,2 +1,9 @@
 import { handlers } from "@/auth"; // Referring to the auth.ts we just created
+import { NextRequest, NextResponse } from "next/server";
+
 export const { GET, POST } = handlers;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const HEAD = async (req: NextRequest) => {
+  return NextResponse.json({ message: "OK" }, { status: 200 });
+};
