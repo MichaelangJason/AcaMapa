a0725604a3040e7d3f9e781cd4b8fd3d631aafa5
mkljs <mkl.js@outlook.com>
2025-09-10 11:41:38 -0400

    chore: update lint-staged script

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,6 +2,7 @@ export default {
   "src/**/*.{js,ts,tsx,jsx,css,scss}": (stagedFiles) => [
     `gitleaks git --staged`,
     `next lint --file ${stagedFiles.join(" ")}`,
+    `pnpm build`,
     `prettier --write ${stagedFiles.join(" ")}`,
     `git add ${stagedFiles.join(" ")}`,
   ],
