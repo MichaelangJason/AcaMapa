e3451b27b5b24a33b95930ff90f7f4535ee5df25
mkljs <mkl.js@outlook.com>
2025-11-09 23:18:57 -0500

    chore: update to nextjs16, cache components not added yet

--- next.config.ts
+++ next.config.ts
@@ -7,13 +7,25 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     qualities: [100]
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"], // https://stackoverflow.com/questions/65676689/next-js-how-can-i-change-the-color-of-svg-in-next-image
-    });
-    return config;
+  outputFileTracingRoot: './',
+  turbopack: {
+    // https://nextjs.org/docs/app/api-reference/config/next-config-js/turbopack#configuring-webpack-loaders
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js'
+      }
+    }
   },
+  cacheComponents: true
+  
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ["@svgr/webpack"], // https://stackoverflow.com/questions/65676689/next-js-how-can-i-change-the-color-of-svg-in-next-image
+  //   });
+  //   return config;
+  // },
 };
 
 // Add bundle analyzer
