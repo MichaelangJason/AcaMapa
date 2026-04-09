b970a9210ebee019275c36de930edc6f346c4c2f
mkljs <mkl.js@outlook.com>
2026-04-09 16:20:04 -0400

    feat: add typed useStore hook

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,5 +1,6 @@
-import { useDispatch, useSelector } from "react-redux";
-import type { AppDispatch, RootState } from ".";
+import { useDispatch, useSelector, useStore } from "react-redux";
+import type { AppDispatch, RootState, AppStore } from ".";
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppStore = useStore.withTypes<AppStore>();
